0215f729e438fb80aec1ea8103cd22b44d2e19f0
Mack Thompson <mackthompson16@gmail.com>
2025-12-01 23:42:18 -0600

    december update

--- src/App.js
+++ src/App.js
@@ -158,7 +158,7 @@ const buildHref = (item) => {
     );
   })}
 </nav>
-<div className="footer"><p>Last Updated 10/29/2025</p></div>
+<div className="footer"><p>Last Updated 12/1/2025</p></div>
     </div>
   );
 }
